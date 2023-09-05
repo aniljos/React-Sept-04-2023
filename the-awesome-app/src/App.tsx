be71b24723a8e90025f11fa672372f1f3d697df8
aniljos <anil.jos@gmail.com>
2023-09-05 15:31:20 +0530

    Day 2

--- the-awesome-app/src/App.tsx
+++ the-awesome-app/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import Hello from './components/Hello';
 import Counter from './components/Counter';
 import FnCounter from './components/FnCounter';
+import ListProducts from './components/ListProducts';
 
 function App() {
   return (
@@ -16,10 +17,12 @@ function App() {
           {/* <Hello message="Hello React"/>
           <Hello message="Welcome React"/>
           <Counter initValue={10}/> */}
-          <Counter initValue={15}/>
+          {/* <Counter initValue={15}/>
 
           <FnCounter initValue={5}/>
-          <FnCounter />
+          <FnCounter /> */}
+
+          <ListProducts/>
  
       </section>
     </div>
---
+++ the-awesome-app/src/components/ListProducts.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import { Product } from '../model/Product';
+import './ListProducts.css';
+
+function ListProducts() {
+
+    const [products, setProducts] = useState<Product[]>([])
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+
+    async function fetchProducts() {
+
+        const url = "http://localhost:9000/products";
+        // axios
+        //     .get(url)
+        //     .then((response)=> {
+        //         console.log("response", response);
+        //     }, (error) => {
+        //         console.log("error", error);
+        //     })
+
+        try {
+
+            const response = await axios.get(url);
+            console.log("response", response);
+            setProducts(response.data);
+
+        } catch (error) {
+            console.log("error", error);
+        }
+    }
+
+    async function remove(item:Product){
+
+        try {
+            const url = "http://localhost:9000/products/" + item.id;
+            await axios.delete(url);
+            fetchProducts();
+            alert("Record deleted");
+        } catch (error) {
+            alert("Failed to delete record");
+        }
+
+    }
+
+    return (
+        <div>
+            <h3>List Products</h3>
+            <div style={{display: "flex", flexFlow: 'row wrap', justifyContent: 'center'}}>
+                {products.map((item, index) => {
+                    return (
+                        <div key={index} className="product">
+                            <p>Id: {item.id}</p>
+                            <p>{item.name}</p>
+                            <p>{item.description}</p>
+                            <p>Price: {item.price}</p>
+                            <div>
+                                <button onClick={() => {remove(item)}}>Delete</button> &nbsp;
+                                <button>Edit</button>
+                            </div>
+
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default ListProducts;
